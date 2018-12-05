263218676d29820cb74c381ad20ffd3c7557f48d
everywill <everywillatchina@gmail.com>
2018-12-05 22:38:37 +0800

    lexer complete

--- Lexer.js
+++ Lexer.js
@@ -17,28 +17,37 @@ class Lexer {
 
     let pos = 0
     let endPos = line.length
-    console.log('endPos: ', endPos)
     let Regexp = new RegExp(regexPat, 'g')
     while (pos < endPos) {
       const matcher = Regexp.exec(line)
       pos = Regexp.lastIndex
       if (matcher) {
-        console.log('pos: ', pos)
-        console.log('endPos: ', endPos)
         this.addToken(this.lineNo, matcher)
       } else {
         throw new Error(`bad token at line: ${this.lineNo} near postion: ${pos}`)
       }
     }
+    console.log('Token queue: ', this.queue)
   }
-  
   addToken(lineNo, matcher) {
-    console.log(matcher)
-    this.queue.push({
-      lineNo,
-      // value:
-      // type: 
-    })
+    const token = { lineNo }
+    // console.log(matcher)
+    if (matcher[2] !== undefined) {
+      token.type = 'number'
+      token.value = parseInt(matcher[2])
+    } else if (matcher[3] !== undefined) {
+      token.type = 'string'
+      token.value = matcher[3]
+    } else if (matcher[4] !== undefined){
+      token.type = 'identity'
+      token.value = matcher[4]
+    }
+    if (matcher[1] === undefined) {
+      this.queue.push(token)
+    }
+  }
+  getToken(index) {
+    return this.queue[index]
   }
   read() {
     this.reader.on('line', this.lineNumberReader.bind(this))
