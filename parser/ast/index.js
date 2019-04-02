46ccd1e5a1f5f314e058bad75805cc577101dd7e
徐承 <xucheng.pc@alibaba-inc.com>
2019-04-02 21:09:16 +0800

    add new ast node

--- parser/ast/index.js
+++ parser/ast/index.js
@@ -47,6 +47,15 @@ class ASTList {
   }
 }
 
+class StringLiteral extends ASTLeaf {
+  constructor(token) {
+    super(token)
+  }
+  get value() {
+    return this.token.value
+  }
+}
+
 class NumberLiteral extends ASTLeaf {
   constructor(token) {
     super(token)
@@ -70,18 +79,74 @@ class BinaryExpr extends ASTList {
     super(tokenList)
   }
   get left() {
-    return this.children(0)
+    return this.child(0)
   }
   get operator() {
-    return this.children(1)
+    return this.child(1)
   }
   get right() {
-    return this.children(2)
+    return this.child(2)
+  }
+}
+
+class NegativeExpr extends ASTList {
+  constructor(tokenList) {
+    super(tokenList)
+  }
+  get operand() {
+    return this.child(1)
+  }
+  toString() {
+    return `-${this.operand()}`
+  }
+}
+
+class BlockStmnt extends ASTList {
+  constructor(tokenList) {
+    super(tokenList)
+  }
+}
+
+class IfStmnt extends ASTList {
+  constructor(tokenList) {
+    super(tokenList)
+  }
+  get condition() {
+    return this.child(0)
+  }
+  get thenBlock() {
+    return this.child(1)
+  }
+  get elseBlock() {
+    return this.numChildren() > 2 ? this.child(2) : null
+  }
+  toString() {
+    return `(if ${this.condition} ${this.thenBlock} else ${this.elseBlock})`
+  }
+}
+
+class WhileStmnt extends ASTList {
+  constructor(tokenList) {
+    super(tokenList)
+  }
+  get condition() {
+    return this.child(0)
+  }
+  get body() {
+    return this.child(1)
+  }
+  toString() {
+    return `(while ${this.condition} ${this.body})`
   }
 }
 
 module.exports = {
+  StringLiteral,
   NumberLiteral,
   Name,
   BinaryExpr,
+  NegativeExpr,
+  BlockStmnt,
+  IfStmnt,
+  WhileStmnt,
 }
