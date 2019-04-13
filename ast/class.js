29175e04de66aa869e56ac570dd5d851ced7ff74
徐承 <xucheng.pc@alibaba-inc.com>
2019-04-14 00:14:25 +0800

    modify class ast

--- ast/class.js
+++ ast/class.js
@@ -1,9 +1,16 @@
 const { ASTList } = require('./root-type')
 
+class ClassInfo {
+  constructor(classStmnt, env) {
+    
+  }
+}
+
 class ClassBody extends ASTList {
   constructor(tokenList) {
     super(tokenList)
   }
+  eval(env) {}
 }
 
 class ClassStmnt extends ASTList {
@@ -13,15 +20,39 @@ class ClassStmnt extends ASTList {
   get name() {
     return this.child(0).name
   }
+  get superClass() {
+    if (this.numChildren < 3) {
+      return null
+    }
+    return this.child(1).name
+  }
   get body() {
     return this.child(this.numChildren - 1)
   }
   toString() {
-    
+    let parent = this.superClass
+    parent = parent || '*'
+
+    return `(class ${this.name} ${parent} ${this.body})`
   }
+  eval(env) {}
+}
+
+class Dot extends ASTList {
+  constructor(tokenList) {
+    super(tokenList)
+  }
+  get name() {
+    return this.child(0).name
+  }
+  toString() {
+    return `.${this.name}`
+  }
+  eval(env, target) {}
 }
 
 module.exports = {
   ClassBody,
   ClassStmnt,
+  Dot,
 }
