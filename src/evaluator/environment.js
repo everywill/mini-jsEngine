8902b24a6222ee658d6b8448185e11e2044c789d
徐承 <xucheng.pc@alibaba-inc.com>
2019-04-30 18:36:38 +0800

    add arraySymbolEnv

--- src/evaluator/environment.js
+++ src/evaluator/environment.js
@@ -105,7 +105,49 @@ class ArrayEnv extends EnvWithNatives {
 class ArraySymbolEnv extends ArrayEnv {
   constructor(env) {
     super(env)
-    this.symbols = new Symbols()
+    this.names = new Symbols()
+  }
+  get(...args) {
+    if (args.length === 2) {
+      // args = [nestHierarchy, index]
+      return super.get(...args)
+    }
+    // args = [name]
+    let name = args[0]
+    let index = this.names.find(name)
+    if (index === -1) {
+      if (this.outer) {
+        return this.outer.get(name)
+      }
+      return null
+    }
+    return this.values[index]
+  }
+  put(...args) {
+    if (args.length === 3) {
+      // args = [nestHierarchy, index, value]
+      super.put(...args)
+    } else {
+      let [name, value] = args
+      let e = this.where(name)
+      if (!e) {
+        e = this
+      }
+      e.putNew(name, value)
+    }
+  }
+  putNew(name, value) {
+    let index = this.names.putNew(name)
+    this.values[index] = value
+  }
+  where(name) {
+    let index = this.names.find(name)
+    if (index !== -1) {
+      return this
+    } else if (this.outer) {
+      return this.outer.where(name)
+    }
+    return null
   }
 }
 
