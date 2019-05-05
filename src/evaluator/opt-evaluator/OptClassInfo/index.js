5d0ff35675fea78eb23259205ccb6d14cae553f0
徐承 <xucheng.pc@alibaba-inc.com>
2019-05-05 08:15:00 +0800

    OptStoneObject

--- src/evaluator/opt-evaluator/OptClassInfo/index.js
+++ src/evaluator/opt-evaluator/OptClassInfo/index.js
@@ -1,3 +1,23 @@
-class OptClassInfo {}
+const ClassInfo = require('../../class-evaluator/ClassInfo')
+const OptMethod = require('../OptMethod')
+
+class OptClassInfo extends ClassInfo {
+  constructor(classStmnt, env, methodsSyms, fieldsSyms) {
+    super(classStmnt, env)
+    this.methodsSyms = methodsSyms
+    this.fieldsSyms = fieldsSyms
+    this.methodDefs = []
+  }
+  fieldIndex(name) {
+    return this.fieldsSyms.find(name)
+  }
+  methodIndex(name) {
+    return this.methodsSyms.find(name)
+  }
+  method(optStoneObject, index) {
+    let methodDef = this.methodDefs[index]
+    return new OptMethod(methodDef.parameters,methodDef.body, this.environment, optStoneObject)
+  }
+}
 
 module.exports = OptClassInfo
--- src/evaluator/opt-evaluator/OptStoneObject/index.js
+++ src/evaluator/opt-evaluator/OptStoneObject/index.js
@@ -1,3 +1,36 @@
-class OptStoneObject {}
+class OptStoneObject {
+  constructor(optClassInfo) {
+    this.optClassInfo = optClassInfo
+    this.fields = []
+  }
+  read(mark) {
+    if (typeof mark === 'number') {
+      return this.fields[mark]
+    }
+    let index = this.optClassInfo.fieldIndex(mark)
+    if (index !== -1) {
+      return this.fields[index]
+    }
+    index = this.optClassInfo.methodIndex(mark)
+    if (index !== -1) {
+      return this.method(index)
+    }
+    throw new Error(`bad access: ${mark}`)
+  }
+  write(mark, value) {
+    if (typeof mark === 'number') {
+      this.fields[mark] = value
+    }
+    let index = this.optClassInfo.fieldIndex(mark)
+    if (index !== -1) {
+      this.fields[index] = value
+    } else {
+      throw new Error(`bad assign: ${mark}`)
+    }
+  }
+  method(index) {
+    return this.optClassInfo.method(this, index)
+  }
+}
 
 module.exports = OptStoneObject
